8c24a16b67aaeb5d94e199d5773557a1012e5f5d
mastodon <mastodon@tsuki.network>
2019-03-04 01:26:28 +0900

    Initial Commit

---
+++ restart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo systemctl stop mastodon-streaming.service mastodon-sidekiq.service mastodon-web.service
+sudo systemctl start mastodon-web.service mastodon-sidekiq.service mastodon-streaming.service
+sudo systemctl status mastodon-web.service mastodon-sidekiq.service mastodon-streaming.service --no-pager
+
---
+++ upgrade.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export RAILS_ENV=production
+pushd /home/mastodon/live > /dev/null
+git fetch --all
+git checkout tsuki.network > /dev/null 2>&1
+git status | grep "up to date"
+if [[ $? -eq 0 ]]; then exit 0; fi
+git reset --hard origin/tsuki.network
+
+rbenv versions | grep $(cat .ruby-version)
+if [[ $? -ne 0 ]]; then
+  pushd /home/mastodon/.rbenv/plugins/ruby-build
+  git pull
+  popd
+  rbenv install $(cat .ruby-version)
+  rbenv global $(cat .ruby-version)
+fi
+
+gem install bundler
+bundle install -j$(getconf _NPROCESSORS_ONLN) --deployment --without development test
+yarn install --pure-lockfile
+SKIP_POST_DEPLOYMENT_MIGRATIONS=true bundle exec rails db:migrate
+sudo systemctl stop mastodon-streaming.service mastodon-sidekiq.service mastodon-web.service
+bundle exec rails assets:precompile
+sudo systemctl start mastodon-web.service mastodon-sidekiq.service mastodon-streaming.service
+bundle exec rails db:migrate
+popd > /dev/null
+
+sudo systemctl status mastodon-web.service mastodon-sidekiq.service mastodon-streaming.service --no-pager
+
