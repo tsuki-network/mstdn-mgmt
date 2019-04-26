38a8d859da9b84341c3d8bcfcbd7791e9f7d7b1c
Satsuki Yanagi <17376330+u1-liquid@users.noreply.github.com>
2019-04-26 20:39:31 +0900

    Send notice message after upgrade ruby/node

--- upgrade.sh
+++ upgrade.sh
@@ -10,9 +10,6 @@ git status | grep "up to date"
 if [[ $? -eq 0 ]]; then exit 0; fi
 git reset --hard origin/$SERVICE_BRANCH
 
-MSTDN_UPGRADE_VERSION=$(git describe --tags --exact-match || echo "$(git describe --tags $(git rev-list --tags --max-count=1)) ($(git rev-parse HEAD))")
-/home/mastodon/toot 【メンテナンス告知】当インスタンスは、今から約30分間 Mastodon $MSTDN_UPGRADE_VERSION へのアップデートを行います。その間、アクセスが円滑でないことがありますので、ご了承お願いいたします。
-
 rbenv versions | grep $(cat .ruby-version)
 if [[ $? -ne 0 ]]; then
   pushd /home/mastodon/.rbenv/plugins/ruby-build
@@ -24,6 +21,9 @@ fi
 . ~/.nvm/nvm.sh install $(cat .nvmrc)
 . ~/.nvm/nvm.sh use $(cat .nvmrc)
 npm install -g npm
+
+MSTDN_UPGRADE_VERSION=$(git describe --tags --exact-match || echo "$(git describe --tags $(git rev-list --tags --max-count=1)) ($(git rev-parse HEAD))")
+/home/mastodon/toot 【メンテナンス告知】当インスタンスは、今から約30分間 Mastodon $MSTDN_UPGRADE_VERSION へのアップデートを行います。その間、アクセスが円滑でないことがありますので、ご了承お願いいたします。
 
 gem install bundler
 bundle install -j$(getconf _NPROCESSORS_ONLN) --deployment --without development test
