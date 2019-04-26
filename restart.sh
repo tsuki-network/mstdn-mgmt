ff7c3de9be17b3b257c3db7a2d0db6694e3e4872
mastodon <mastodon@tsuki.network>
2019-04-26 18:54:42 +0900

    Automate restart / upgrade Notice

--- restart.sh
+++ restart.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+/home/mastodon/toot 【メンテナンス告知】当インスタンスは、今から10秒後、約1分間 Mastodon サービスの再起動を行います。その間、アクセスが円滑でないことがありますので、ご了承お願いいたします。
+sleep 10s
+
 sudo systemctl stop mastodon-streaming.service mastodon-sidekiq.service mastodon-web.service
 sudo systemctl start mastodon-web.service mastodon-sidekiq.service mastodon-streaming.service
-sudo systemctl status mastodon-web.service mastodon-sidekiq.service --full --no-pager mastodon-streaming.service
+sudo systemctl status --full --no-pager mastodon-web.service mastodon-sidekiq.service mastodon-streaming.service
+
+while ! curl -sSLI https://tsuki.network/.well-known/host-meta -o /dev/null -w '%{http_code}' | grep '200' | wc -l; do
+  sleep 1s
+done
+
+/home/mastodon/toot 【メンテナンス終了】Mastodon サービスの再起動が完了しました。
 
--- upgrade.sh
+++ upgrade.sh
@@ -8,6 +8,9 @@ git status | grep "up to date"
 if [[ $? -eq 0 ]]; then exit 0; fi
 git reset --hard origin/tsuki.network
 
+MSTDN_UPGRADE_VERSION=$(git describe --tags --exact-match || echo "$(git describe --tags $(git rev-list --tags --max-co unt=1)) ($(git rev-parse HEAD))")
+/home/mastodon/toot 【メンテナンス告知】当インスタンスは、今から約30分間 Mastodon $MSTDN_UPGRADE_VERSION へのアップデートを行います。その間、アクセスが円滑でないことがありますので、ご了承お願いいたします。
+
 rbenv versions | grep $(cat .ruby-version)
 if [[ $? -ne 0 ]]; then
   pushd /home/mastodon/.rbenv/plugins/ruby-build
@@ -31,4 +34,10 @@ bundle exec rails db:migrate
 popd > /dev/null
 
 sudo systemctl status --full --no-pager mastodon-web.service mastodon-sidekiq.service mastodon-streaming.service
+
+while ! curl -sSLI https://tsuki.network/.well-known/host-meta -o /dev/null -w '%{http_code}' | grep '200' | wc -l; do
+  sleep 1s
+done
+
+/home/mastodon/toot 【メンテナンス終了】Mastodon $MSTDN_UPGRADE_VERSION へのアップデートが完了しました。
 
