5e02bb37bbf137d7d7a4cba7341c4c5f1e2c6a2d
Satsuki Yanagi <17376330+u1-liquid@users.noreply.github.com>
2019-08-25 09:26:35 +0900

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -28,7 +28,7 @@ MSTDN_UPGRADE_VERSION=$(git describe --tags --exact-match || echo "$(git describ
 /home/mastodon/toot 【メンテナンス告知】当インスタンスは、今から約30分間 Mastodon $MSTDN_UPGRADE_VERSION へのアップデートを行います。その間、アクセスが円滑でないことがありますので、ご了承お願いいたします。
 
 gem update --system
-gem install bundler
+gem install bundler --no-document
 bundle install -j$(getconf _NPROCESSORS_ONLN) --deployment --without development test
 yarn install --pure-lockfile
 SKIP_POST_DEPLOYMENT_MIGRATIONS=true bundle exec rails db:migrate
